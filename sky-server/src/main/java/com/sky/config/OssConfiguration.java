3aa2ab51e73ea8c2464cb7632dd89a773c28d8cc
cds-dn-181 <417074596@qq.com>
2024-09-10 17:56:49 +0800

    更新

--- sky-server/src/main/java/com/sky/config/OssConfiguration.java
+++ sky-server/src/main/java/com/sky/config/OssConfiguration.java
@@ -19,5 +19,4 @@ public class OssConfiguration {
         log.info("开始创建阿里云上传工具类对象：{}",aliOssProperties);
         return new AliOssUtil(aliOssProperties.getEndpoint(), aliOssProperties.getAccessKeyId(), aliOssProperties.getAccessKeySecret(), aliOssProperties.getBucketName());
     }
-
 }
--- sky-server/src/main/java/com/sky/mapper/UserMapper.java
+++ sky-server/src/main/java/com/sky/mapper/UserMapper.java
@@ -19,12 +19,4 @@ public interface UserMapper {
      * @param user
      */
     void insert(User user);
-
-    /**
-     * 根据用户id查询用户
-     * @param userId
-     * @return
-     */
-    @Select("select * from user where id = #{id}")
-    User getById(Long userId);
 }
